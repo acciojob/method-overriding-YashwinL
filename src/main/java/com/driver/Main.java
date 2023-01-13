af8bdae3460e500980494cfdaf959d71b14a1176
YashwinL <yashwinlakshmipathy@gmail.com>
2023-01-13 12:03:12 +0530

    Made Changes override 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,20 @@
 package com.driver;
 
 public class Main {
+
+    public static  class A{
+
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static  class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
@@ -9,15 +23,3 @@ public class Main {
     }
 }
 
-class A{
- public String meth(){
-     return "Invoking method from class A";
- }
-}
-
-class B extends A{
-    @Override
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
